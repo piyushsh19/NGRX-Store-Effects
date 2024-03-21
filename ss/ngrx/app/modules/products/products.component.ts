98184434489e8ed5640aba1eff7b9c00c42ff183
piyushsh19 <piyush.sh19@gmail.com>
2024-03-21 22:23:59 +0530

    product updated

--- ss/ngrx/app/modules/products/products.component.ts
+++ ss/ngrx/app/modules/products/products.component.ts
@@ -1,60 +1,28 @@
+import { Product } from './product.model';
+import { AppState } from './../app.state';
 import { Component, OnInit } from '@angular/core';
-import * as fromProductModels from '../products/resources/product';
-import { environment } from 'src/environments/environment';
-import { PaginationService } from 'src/app/shared/services/pagination.service';
-import * as fromProductActions from './state/product.actions';
-import { Store, select } from '@ngrx/store';
-import { AppState } from 'src/app/store';
-import * as ProductSelector from './state/product.selectors';
+import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-
 @Component({
-  selector: 'app-products',
-  templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss'],
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
 })
-export class ProductsComponent implements OnInit {
-  vm$: Observable<ProductSelector.ProductsViewModel>;
-
-  constructor(
-    private paginationService: PaginationService,
-    private store: Store<AppState>
-  ) {}
-
+export class ProductComponent implements OnInit {
+  products: Observable<Product[]>;
+  constructor(private store: Store<AppState>) {
+    this.products = this.store.select(state => state.product);
+    console.log(" this.products", this.products)
+   }
+  addProduct(name:any, price:any) {
+    this.store.dispatch({
+      type: 'ADD_PRODUCT',
+      payload: <Product> {
+        name: name,
+        price: price
+      }
+    });
+  }
   ngOnInit() {
-    this.vm$ = this.store.pipe(select(ProductSelector.selectProductsViewModel));
-    this.loadProducts(
-      this.paginationService.createUrl(
-        '0',
-        '999',
-        '1',
-        '9',
-        environment.baseUrl + 'products?'
-      )
-    );
-  }
-
-  loadProducts(url: string) {
-    this.store.dispatch(
-      fromProductActions.loadProducts({
-        url: url,
-      })
-    );
-  }
-
-  onPriceFilterChange(item: fromProductModels.PriceFilter) {
-    this.loadProducts(
-      this.paginationService.createUrl(
-        item.min,
-        item.max,
-        '1',
-        '25',
-        environment.baseUrl + 'products?'
-      )
-    );
-  }
-
-  onPaginationChange(url: string) {
-    this.loadProducts(url);
   }
 }
